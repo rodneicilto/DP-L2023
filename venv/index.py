29523b094d27103a2942199db6f88843dc8e5016
Rodnei <rodnei.cilto@gmail.com>
2023-05-24 20:37:48 -0300

    New deploy 2

--- venv/index.py
+++ venv/index.py
@@ -6,23 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_tamplate('/html/index.html')
-
+    return render_template('/html/index.html')
+if __name__ == '__main__':
+    app.run()
 
 @app.route('/html/about')
 def about():
     return 'About Page Route'
 
-
 @app.route('/html/portfolio')
 def portfolio():
     return 'Portfolio Page Route'
 
-
 @app.route('/html/contact')
 def contact():
     return 'Contact Page Route'
-
 
 @app.route('/api/src')
 def api():
