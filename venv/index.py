5b840f6ac78d0a725c0b314cff2d5f75c37eb9b9
Rodnei <rodnei.cilto@gmail.com>
2023-05-24 20:56:30 -0300

    Deploy teste 2

--- venv/index.py
+++ venv/index.py
@@ -6,23 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('html/index.html')
+    return render_template('/html/index.html')
 if __name__ == '__main__':
     app.run()
 
-@app.route('html/about')
+@app.route('/html/about')
 def about():
     return 'About Page Route'
 
-@app.route('html/portfolio')
+@app.route('/html/portfolio')
 def portfolio():
     return 'Portfolio Page Route'
 
-@app.route('html/contact')
+@app.route('/html/contact')
 def contact():
     return 'Contact Page Route'
 
-@app.route('api/src')
+@app.route('/api/src')
 def api():
     with open('data.json', mode='r') as my_file:
         text = my_file.read()
