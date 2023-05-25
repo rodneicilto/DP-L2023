c66901296ff3c7a08543b67b4444669788d6e94b
Rodnei <rodnei.cilto@gmail.com>
2023-05-24 22:11:04 -0300

    Deploy

--- venv/index.py
+++ venv/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder='template', static_folder='template/static')
+app = Flask(__name__, template_folder='template', static_folder='static')
 
 @app.route('/')
 def home():
