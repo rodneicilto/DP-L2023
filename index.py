1d4a592bb15d7d2b718293025f6c225bb8b561fc
rodneicilto <rodnei.cilto@gmail.com>
2023-05-26 19:40:26 -0300

    Save txt with form

--- index.py
+++ index.py
@@ -14,8 +14,9 @@ def home():
         input_startD = request.form['dateS_input']
         input_endD = request.form['dateE_input']
         if request.method == 'POST':
-            f.write(str(input_name,input_email,input_sof,input_startD,input_endD))
-            return render_template("index.html", nopol=input_name)
+            with open('nopol.txt', 'w') as f:
+                f.write(str(input_name,input_email,input_sof,input_startD,input_endD))
+        return render_template("index.html", nopol=input_name)
 
 @app.route('/about')
 def about():
