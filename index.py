6091d75a9e1543b8966b1efc5f3ada8d8d15d498
rodneicilto <rodnei.cilto@gmail.com>
2023-05-31 16:29:42 -0300

    teste online

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from os import listdir
+#from gevent.pyswgi import WSGIServer
 
 app = Flask(__name__, template_folder='template', static_folder='static')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/', methods=['post', 'get'])
+def form_home():
+    if request.method == 'post':
+<<<<<<< HEAD
+        with open('nopol_1.txt', 'w') as f:
+            f.write(str(input_name))
+    return render_template("index.html", nopol_1=input_name)
 
 @app.route('/about')
 def about():
@@ -18,6 +27,15 @@ def portfolio():
 @app.route('/contact')
 def contact():
     return 'Contact Page Route'
+=======
+        print(request.form)
+        print(request.form['name_input'])
+        print(request.form['email_input'])
+        print(request.form['sof_input'])
+        print(request.form['dateS_input'])
+        print(request.form['dateE_input'])
+    return request.form['name_input'] + request.form['email_input']
+>>>>>>> ebfbb945 (test online)
 
 @app.route('/api/src')
 def api():
