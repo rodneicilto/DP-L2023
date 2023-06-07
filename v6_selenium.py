ef2207469a77ec9087028beae0465e1e5903ffb0
Rodnei <rodnei.cilto@gmail.com>
2023-06-07 20:58:19 -0300

    new teste

--- v6_selenium.py
+++ v6_selenium.py
@@ -5,7 +5,7 @@ import send_email
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
-from sebdriver_manager,chrome import ChromeDriverManager
+from sebdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
