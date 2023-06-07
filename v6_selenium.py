7174e303d4b1efa46ffeea80aef73dd50913b623
Rodnei <rodnei.cilto@gmail.com>
2023-06-07 20:10:18 -0300

    new teste

--- v6_selenium.py
+++ v6_selenium.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pyvirtualdisplay import Display
 import requests
 import re
 import json
@@ -34,14 +35,17 @@ def web_scraping(v_sw, v_dateS, v_dateE, name_fsite, email_fsite):
     final_date = (v_dateE)
 
     try:
-        op = webdriver.ChromeOptions()
+        #op = webdriver.ChromeOptions()
         #op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        op.add_argument("--headless")
-        op.add_argument("--disable-dev-shm-usage")
-        op.add_argument("--no-sandbox")
-        browser = webdriver.Chrome(driver_path='chromedriver', chrome_options=op, services_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+        #op.add_argument("--headless")
+        #op.add_argument("--disable-dev-shm-usage")
+        #op.add_argument("--no-sandbox")
+        #browser = webdriver.Chrome(driver_path='chromedriver', chrome_options=op, services_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
         #browser.maximize_window()
         #browser.minimize_window()
+        display = Display(visible=0, size=(1024, 768))
+        display.start()
+        driver = webdriver.Chrome(driver_path='chromedirver', service_args=['--verbose','--log-path=/tmp/chromedriver.log']
 
         browser.get('https://nvd.nist.gov/vuln/search')
         link = browser.find_element(By.ID, 'Keywords').send_keys(vulnerability)
