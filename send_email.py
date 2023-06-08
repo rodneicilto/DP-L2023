c8d03f12b776ff35224300b426e5b4678a21df1a
Rodnei <rodnei.cilto@gmail.com>
2023-06-07 22:19:35 -0300

    Test

--- send_email.py
+++ send_email.py
@@ -4,6 +4,10 @@ import pandas as pd
 import email.message
 from email import encoders
 from datetime import datetime
+from email.mime.applications import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
 
 def send(email_dest,name_file):
     bufferSize = 64 * 1024
@@ -15,9 +19,18 @@ def send(email_dest,name_file):
     msg['Subject'] = "Vulnerabilidades Críticas Data"+name_file
     msg['From'] = 'valimfabiano@gmail.com'
     msg['To'] = (email_dest)
-    password = 'cpzocyejvwzwgagv' 
-    msg.add_header('Content-Type', 'text/html')
+    password = 'cpzocyejvwzwgagv'
+    #msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email)
+    
+    msg.attach(MIMEText(body, 'plain'))
+    filename = name_file+_"vulnerability.xls"
+    attachment = open("/tmp/")
+
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename = %s" % filename)
 
     s = smtplib.SMTP('smtp.gmail.com: 587')
     s.starttls()
