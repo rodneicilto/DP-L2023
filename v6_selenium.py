b8cdf2253fb3cf9749abeb7a03886f27f94b5203
Rodnei <rodnei.cilto@gmail.com>
2023-06-08 22:09:45 -0300

    New teste

--- v6_selenium.py
+++ v6_selenium.py
@@ -16,7 +16,7 @@ import re
 import json
 
 def web_scraping(v_sw, v_dateS, v_dateE, name_fsite, email_fsite):
-                
+                    
     #if os.path.exists("Vulnerability.xls"):
     #os.remove("Vulnerability.xls")
     table = pd.DataFrame({'Software/System': [], 'CVE': [], 'Current Description': [], 'Severity': [], 'References': [], 'Afected Software': [], 'NVD Date': [], 'Link to Issue': [] })
@@ -43,7 +43,7 @@ def web_scraping(v_sw, v_dateS, v_dateE, name_fsite, email_fsite):
         #op.add_argument("--disable-dev-shm-usage")
         #op.add_argument("--no-sandbox")
         browser = webdriver.Chrome(options=op)
-
+        
         browser.get('https://nvd.nist.gov/vuln/search')
         link = browser.find_element(By.ID, 'Keywords').send_keys(vulnerability)
         search_type = browser.find_element(By.ID, 'SearchTypeAdvanced').click()
@@ -57,10 +57,10 @@ def web_scraping(v_sw, v_dateS, v_dateE, name_fsite, email_fsite):
             vuln_researched = vulnerability
             vuln_id = browser.find_element('xpath', '/html/body/main/div/div/div[2]/div[2]/table/tbody/tr/td/div/div[2]/div/a').text
             vuln_description = browser.find_element('xpath', '/html/body/main/div/div/div[2]/div[2]/table/tbody/tr/td/div/div[1]/p').text
-
+            
             try:
                 vuln_severity = browser.find_element(By.ID, 'Cvss3NistCalculatorAnchor').text
-
+                
             except:
                 vuln_severity = browser.find_element(By.ID, 'Cvss3NistCalculatorAnchorNA').text
                 vuln_reference = browser.find_element(By.CLASS_NAME, 'external').text
@@ -72,7 +72,7 @@ def web_scraping(v_sw, v_dateS, v_dateE, name_fsite, email_fsite):
                 vuln_affected = ('No Software was informed related to this issue')
                 vuln_date = browser.find_element('xpath', '/html/body/main/div/div/div[2]/div[2]/table/tbody/tr/td/div/div[2]/div/span[1]').text
                 vuln_link = f'https://nvd.nist.gov/vuln/detail/{vuln_id}'
-
+            
             print('1parte-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+')
             #print('')
             #print('Data Returned......')
@@ -86,12 +86,14 @@ def web_scraping(v_sw, v_dateS, v_dateE, name_fsite, email_fsite):
             #print('Link to Issue: ', vuln_link)
             #print('')
             #print('-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+')
+            
             table = pd.DataFrame({'Software/System': [vulnerability], 'CVE': [vuln_id], 'Current Description': [vuln_description], 'Severity': [vuln_severity], 'References': [vuln_reference], 'Afected Software': [vuln_affected], 'NVD Date': [vuln_date], 'Link to Issue': [vuln_link] })
             reader = pd.read_excel('/tmp/'+name_fsite+'_vulnerability.xls')
             writer = pd.ExcelWriter('/tmp/'+name_fsite+'_vulnerability.xls', engine='openpyxl', mode='a', if_sheet_exists="overlay")
             table.to_excel(writer, index=False, header=False, startrow=len(reader) + 1)
             writer.close()
             browser.back()
+            print('1-1-+-+')
         time.sleep(10)
         browser.quit()
         send_email_1.send(email_fsite, name_fsite)
