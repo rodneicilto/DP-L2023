a859e7315b24f3e287640c4216a5f07e1e61f3ef
Rodnei <rodnei.cilto@gmail.com>
2023-06-14 22:05:10 -0300

    Adcionado comentarios e removido sujeira do codigo

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ from datetime import datetime, date, timedelta
 from os import listdir
 import send_email 
 import v6_selenium
-#from gevent.pyswgi import WSGIServer
 
 app = Flask(__name__, template_folder='template', static_folder='static')
 app.secret_key="asd"
@@ -22,37 +21,26 @@ def home():
 
 @app.route('/search', methods=['POST','GET'])
 def research():
+    #Dados recebidos do formulario
     flash('Dados inserido, aguarde que logo será enviado o resultado')
-    #if str(request.form['dateS_input']) == '' or str(request.form['dateE_input']) == '':
-        #return render_template("falha.html")
-    #if str(request.form['sof_input']) == '' or str(request.form['email_input']) == '':
-        #return render_template("falha.html")
     name_fsite = str(request.form['name_input'])
     email_fsite = str(request.form['email_input'])
     sof_fsite = str(request.form['sof_input'])
-    
+    #Conversão da data inicio para efetuar a soma dos periodos
     dateS_fsite_T = str(request.form['dateS_input'])
     replace_Test = dateS_fsite_T
     date_object_S = datetime.strptime(replace_Test, '%Y-%m-%d').date()
     dateE_fsite_T = int(request.form['dateE_input'])
-
-    #dateE_fsite_T = str(request.form['dateE_input'])
-    #replace_Test = dateS_fsite_T
-    #date_object_E = datetime.strptime(replace_Test, %m-%d-%Y).date()
-
+    #Remoção do digito '-' para '/' e alteração do posicionamento do ano 
     replace_fsite_E = (date_object_S + timedelta(days=dateE_fsite_T))
     replace_fsite = str(request.form['dateS_input']).split("-")
     dateS_fsite = replace_fsite[1] + '/' + replace_fsite[2] + '/' + replace_fsite[0]
     replace_fconvert = str(replace_fsite_E).split("-")
     dateE_fsite = replace_fconvert[1] + '/' + replace_fconvert[2] + '/' + replace_fconvert[0]
-    
-    #replace_fsite = str(request.form['dateE_input']).split("-")
-    #dateE_fsite = replace_fsite_E[1] + '/' + replace_fsite_E[2] + '/' + replace_fsite_E[0]
-    #list_email = []
-    #list_email.append(email_fsite)
-    #list_email = send_email.send(email_fsite, name_fsite)
+    #Envio das informações para o processo de webscraping 
     list_form_fill = []
     list_form_fill = v6_selenium.web_scraping(sof_fsite, dateS_fsite, dateE_fsite, name_fsite, email_fsite)
+    #Envio das informações coletadas no webscraping por email
     send_email_fill = []
     send_email_fill = send_email.send(email_fsite, name_fsite)
     return render_template('search.html', results=request.form)
